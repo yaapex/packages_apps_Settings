24fe624a5e4afab6855fa5e656498a81cc69fd66
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-23 06:26:56 +0800

    TopLevelSettings: Do not hide the icons on split layout change
    this was causing crashes on tablets and UI inconsistencies
    
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: Process: com.android.settings, PID: 5275
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void com.android.settings.widget.HomepagePreferenceLayoutHelper.setIconVisible(boolean)' on a null object reference
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings.$r8$lambda$Xct_nuHZ2BfaHCvjySE6IhC6Xcs(TopLevelSettings.java:248)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings$$ExternalSyntheticLambda1.doForEach(R8$$SyntheticClass:0)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings.iteratePreferences(TopLevelSettings.java:393)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings.iteratePreferences(TopLevelSettings.java:395)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings.iteratePreferences(TopLevelSettings.java:386)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.TopLevelSettings.onSplitLayoutChanged(TopLevelSettings.java:246)
    03-22 00:48:07.217  5275  5275 E AndroidRuntime: 	at com.android.settings.homepage.SettingsHomepageActivity.lambda$updateSplitLayout$3(SettingsHomepageActivity.java:389)
    
    Change-Id: I8517fb36c8ef9a42b31625f1f11539b09fda7b7a

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -232,13 +232,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     }
 
     @Override
-    public void onSplitLayoutChanged(boolean isRegularLayout) {
-        iteratePreferences(preference -> {
-            if (preference instanceof HomepagePreferenceLayout) {
-                ((HomepagePreferenceLayout) preference).getHelper().setIconVisible(isRegularLayout);
-            }
-        });
-    }
+    public void onSplitLayoutChanged(boolean isRegularLayout) {}
 
     @Override
     public void highlightPreferenceIfNeeded() {
