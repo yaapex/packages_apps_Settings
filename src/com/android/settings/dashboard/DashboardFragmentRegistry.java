RisingOS-staging/android_packages_apps_Settings

Settings: Integrate Personalizations into Settings
Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -50,6 +50,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.rising.settings.PersonalizationsFragment;
+
 import java.util.Map;
 
 /**
@@ -132,6 +134,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_SPECIAL_APP_ACCESS);
         PARENT_TO_CATEGORY_KEY_MAP.put(MoreSecurityPrivacyFragment.class.getName(),
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(PersonalizationsFragment.class.getName(),
+                PersonalizationsFragment.CATEGORY_KEY);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 

yaapex/packages_apps_Settings

Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -50,6 +50,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.custom.settings.PersonalizationsFragment;
+
 import java.util.Map;
 
 /**
@@ -132,6 +134,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_SPECIAL_APP_ACCESS);
         PARENT_TO_CATEGORY_KEY_MAP.put(MoreSecurityPrivacyFragment.class.getName(),
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(PersonalizationsFragment.class.getName(),
+                PersonalizationsFragment.CATEGORY_KEY);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 